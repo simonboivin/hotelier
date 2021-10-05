db7f50cf873c468ee98c3327da295137bf6691db
Simon Boivin <sfp.boivin@gmail.com>
2021-10-05 16:44:27 +0200

    Création Réservation Repository et Service

---
+++ src/main/java/fr/sboivin/hotelier/model/resa/ResaRepository.java
@@ -0,0 +1,6 @@
+package fr.sboivin.hotelier.model.resa;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResaRepository extends CrudRepository<ResaEntity, Integer> {
+}

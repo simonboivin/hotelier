6fa29ed1e2234b84fd92f5e8a9b073fb3369d0d9
Simon Boivin <sfp.boivin@gmail.com>
2021-10-05 17:20:35 +0200

    Création Hotel Controller Api
    manque edit et delete

---
+++ src/main/java/fr/sboivin/hotelier/controllers/api/HotelControllerApi.java
@@ -0,0 +1,51 @@
+package fr.sboivin.hotelier.controllers.api;
+
+
+import fr.sboivin.hotelier.model.hotel.HotelEntity;
+import fr.sboivin.hotelier.model.hotel.HotelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/hotel")
+public class HotelControllerApi {
+
+    private final HotelService hotelService;
+
+    public HotelControllerApi(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping(path = "", produces = "application/json")
+    public Iterable<HotelEntity> getHotelListApi() {
+        return hotelService.getHotelList();
+    }
+
+    @GetMapping(path = "/{id}")
+    public HotelEntity getHotelByIdApi(@PathVariable("id") Integer id) {
+        Optional<HotelEntity> hotelOptional = hotelService.getHotelById(id);
+        if (hotelOptional.isPresent()) {
+            return hotelOptional.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Erreur: L'hôtel n'est pas trouvé");
+        }
+    }
+
+
+    @PostMapping(path = "", produces = "application/json")
+    public ResponseEntity<HotelEntity> addHotelApi(@RequestBody HotelEntity hotelRequestInput) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                hotelService.addHotel(hotelRequestInput.getNom(),
+                        hotelRequestInput.getEtoiles(),
+                        hotelRequestInput.getAdresse(),
+                        hotelRequestInput.getTelephone(),
+                        hotelRequestInput.getEmail(),
+                        hotelRequestInput.getVille())
+        );
+    }
+
+}

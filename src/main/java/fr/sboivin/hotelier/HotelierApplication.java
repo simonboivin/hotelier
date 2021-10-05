56d88f7d8dabfa840d9574c82fefb64830d089a0
Simon Boivin <sfp.boivin@gmail.com>
2021-10-05 11:07:02 +0200

    Initial commit

---
+++ src/main/java/fr/sboivin/hotelier/HotelierApplication.java
@@ -0,0 +1,13 @@
+package fr.sboivin.hotelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/sboivin/hotelier/HotelierApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sboivin.hotelier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

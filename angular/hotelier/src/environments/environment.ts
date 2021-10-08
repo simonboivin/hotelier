df7696e72a04221fe2411676c6003d686e390b3d
Simon Boivin <sfp.boivin@gmail.com>
2021-10-08 11:49:00 +0200

    Creation Entete, Guard  et structure pour le login

--- angular/hotelier/src/environments/environment.ts
+++ angular/hotelier/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  urlBack: "http://localhost:8080/",
+  urlApi: "http://localhost:8080/api/"
 };
 
 /*

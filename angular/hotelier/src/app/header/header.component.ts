29227b98ccad30df44a9cddbcf87f46f3a5bddbd
Simon Boivin <sfp.boivin@gmail.com>
2021-10-11 15:00:32 +0200

    Modification design

--- angular/hotelier/src/app/header/header.component.ts
+++ angular/hotelier/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminEntity } from '../classes/entities/admin-entity';
 import { AuthGuard } from '../guards/auth.guard';
+import { faHSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component( {
   selector: 'app-header',
@@ -10,12 +11,14 @@ import { AuthGuard } from '../guards/auth.guard';
 export class HeaderComponent implements OnInit {
 
   userConnected: AdminEntity = new AdminEntity();
- 
+
+
+  faHSquare = faHSquare;
 
   constructor( public auth: AuthGuard ) { }
 
   ngOnInit (): void {
-       if ( this.auth.isConnected() ) {
+    if ( this.auth.isConnected() ) {
       this.userConnected = JSON.parse( sessionStorage.getItem( "connectedUser" )! );
     }
 
